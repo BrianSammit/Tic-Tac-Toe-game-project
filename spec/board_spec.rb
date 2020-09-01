ff1d4241ef667f08c48b97edf4d7010ad9d9df3f
Sanad <s_wj_88@hotmail.com>
2020-09-02 02:54:34 +0300

    Fix linters

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,76 +1,79 @@
+# rubocop: disable Lint/ParenthesesAsGroupedExpression
+
 require_relative '../lib/board'
 require_relative '../lib/player'
 
 describe Board do
-  context "initialize" do
-    let(:board1) {Board.new}
-    it "Creat the board array" do
+  context 'initialize' do
+    let(:board1) { Board.new }
+    it 'Creat the board array' do
       expect(board1.board).to eq((1..9).to_a)
     end
   end
 
-  context "display_board" do
-    let (:board1) {Board.new}
-    it "Print the board" do
+  context 'display_board' do
+    let (:board1) { Board.new }
+    it 'Print the board' do
       expect(board1.display_board).to eq("    1 | 2 | 3\n    ..|...|..\n    4 | 5 | 6\n    ..|...|..\n    7 | 8 | 9")
     end
   end
 
-  context "full" do
-    let (:board1) {Board.new}
-    it "Check if the board is full" do
+  context 'full' do
+    let (:board1) { Board.new }
+    it 'Check if the board is full' do
       expect(board1.full).to eq(false)
     end
   end
 
-  context "win?" do
-    let (:board1) {Board.new}
-    it "Checks if there is a winner by checking the patterns" do
+  context 'win?' do
+    let (:board1) { Board.new }
+    it 'Checks if there is a winner by checking the patterns' do
       expect(board1.win?).to be false
     end
   end
 
-  context "get_choice" do
-    let (:board1) {Board.new}
+  context 'get_choice' do
+    let (:board1) { Board.new }
 
-    it "Gets user input if is true" do
+    it 'Gets user input if is true' do
       input = 6
       expect(board1.get_choice(input)).to eq(input - 1)
     end
 
-    it "If user input is false" do
+    it 'If user input is false' do
       input = 10
       expect(board1.get_choice(input)).to eq(false)
     end
   end
 
-  context "valid_move" do
-    let (:board1) {Board.new}
-    it "check if the board space not available" do
-      board1.instance_variable_set("@board", ["O", 2, "X", 4, 5, 6, 7, 8, 9] )
+  context 'valid_move' do
+    let (:board1) { Board.new }
+    it 'check if the board space not available' do
+      board1.instance_variable_set('@board', ['O', 2, 'X', 4, 5, 6, 7, 8, 9])
       expect(board1.valid_move(2)).to eq(false)
     end
 
-    it "Check if the board space is available" do
+    it 'Check if the board space is available' do
       player_choice = 4
       expect(board1.valid_move(player_choice)).to eq(true)
     end
   end
 
-  context "update" do
-    let (:board1) {Board.new}
-    let (:token1) {Player.new("gio", "X")}
-    it "Update the player choice with the token" do
-      expect(board1.update(2, token1)).to eq(board1.instance_variable_set("@board", "X" ))
+  context 'update' do
+    let (:board1) { Board.new }
+    let (:token1) { Player.new('gio', 'X') }
+    it 'Update the player choice with the token' do
+      expect(board1.update(2, token1)).to eq(board1.instance_variable_set('@board', 'X'))
     end
   end
 
-  context "switch_player" do
-    let (:board1) {Board.new}
-    it "Change to the next player" do
+  context 'switch_player' do
+    let (:board1) { Board.new }
+    it 'Change to the next player' do
       switch_player = [0, 1]
       expect(board1.switch_player(0, 1)).to eq(1)
     end
   end
 end
 
+# rubocop: enable Lint/ParenthesesAsGroupedExpression
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,17 +1,16 @@
+# frozen_string_literal: true
+
 require_relative '../lib/player'
 
 describe Player do
-  let (:player) {Player.new("gio","X")}
+  let (:player) { Player.new('gio', 'X') }
 
-  context "initialize" do
-    it "Take player name" do
-      expect(player.name).to eq("gio")
+  context 'initialize' do
+    it 'Take player name' do
+      expect(player.name).to eq('gio')
     end
-    it "Set player token" do
-      expect(player.token).to eq("X")
+    it 'Set player token' do
+      expect(player.token).to eq('X')
     end
-
   end
-
-
 end
